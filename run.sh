a776d34f98d543877ddb417db6f5fdf8d93499ea
Samuel Ludwig <SLudwig.dev@gmail.com>
2022-02-15 20:33:58 -0500

    Update run-script and update submodule

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-podman run -it --volume=$(pwd)/cosmic-nvimrc:/root/.config/nvim:Z --volume=$(pwd)/..:/workbench:Z lisp-box
+podman run -it --volume=$(pwd)/cosmic-nvimrc:/root/.config/nvim:Z --volume=./../:/root/workbench:Z lisp-box
